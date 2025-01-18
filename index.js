5813a1474bef0f1305e1d09ec4a6bbc8fc2f5b25
waruna199201 <waruna1992@gmail.com>
2025-01-18 11:42:49 +0530

    added jwt secret on to the emv file 😁

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ app.use((req,res,next)=>{
     if(token != null) {
         token = token.replace("Bearer ","");
         
-        jwt.verify(token, "kv-secret-89!", 
-            (err, decoded) => {
+        jwt.verify(token, ProcessingInstruction.env.JWT_SECRET, (err, decoded) => {
                 
                 if(!err){
                     req.user = decoded;
